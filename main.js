72dcd8a85b11f8b2899bf0911dffba7587ebeb9c
Jueun-Park <elpion19@gmail.com>
2023-08-06 17:36:35 +0900

    add wishes

--- main.js
+++ main.js
@@ -12,6 +12,36 @@ const wishlist = [
         "link": "https://incatos.shop/product/%EB%8F%84%EC%BB%A4-%EC%BB%A8%ED%85%8C%EC%9D%B4%EB%84%88-%EA%B0%81%ED%8B%B0%EC%8A%88-%ED%9C%B4%EC%A7%80-%EC%BC%80%EC%9D%B4%EC%8A%A4-%ED%8B%B0%EC%8A%88%EC%BB%A4%EB%B2%84-%EC%82%AC%EA%B0%81-%EC%95%84%ED%81%AC%EB%A6%B4-%EA%B0%81%ED%8B%B0%EC%8A%88-%EC%BB%A4%EB%B2%84/11/",
         "link_title": "도커 각티슈 케이스 링크"
     },
+    {
+        "completed": false,
+        "name": "종이학 모양 금속 조각",
+        "link": "https://idus.kr/FTjGn",
+        "link_title": "https://idus.kr/FTjGn",
+    },
+    {
+        "completed": false,
+        "name": "약과 모형",
+        "link": "https://idus.kr/F5Sil",
+        "link_title": "https://idus.kr/F5Sil",
+    },
+    {
+        "completed": false,
+        "name": "실리콘 컵받침",
+        "link": "http://kko.to/fFmUboezF0",
+        "link_title": "http://kko.to/fFmUboezF0",
+    },
+    {
+        "completed": false,
+        "name": "악세사리 트레이",
+        "link": "https://idus.kr/F9avO",
+        "link_title": "https://idus.kr/F9avO",
+    },
+    {
+        "completed": false,
+        "name": "조말론 코롱",
+        "link": "http://kko.to/n_Z9jIDCXA",
+        "link_title": "http://kko.to/n_Z9jIDCXA",
+    },
 ]
 
 function shuffle(array) {
@@ -25,7 +55,7 @@ function getWishlist(wishlist) {
             <span class="${wish.completed ? "wishlist completed" : "wishlist"}">
                 ${wish.name}, 예를 들면 이런 것: <a href=${wish.link}>${wish.link_title}</a>
             </span>
-            <span class="thanks">${wish.completed ? "고마워요!" : ""}</span>
+            <small class="thanks">${wish.completed ? "고마워요!" : ""}</small>
         </li>
     `).join('') + "</ul>"
 }
